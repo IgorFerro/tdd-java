a986043b3685a6fc8c4b175a64d9358b948f7b0b
igor.ferro.parreira@gmail.com <igor.ferro.parreira@gmail.com>
2020-09-07 17:05:15 -0300

    Initial commit

---
+++ src/main/java/tdd/java/Dollar.java
@@ -0,0 +1,14 @@
+package tdd.java;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount){
+        this.amount =amount;
+    }
+    void times(int multiplier) {
+        amount *= multiplier;
+    }
+
+}
