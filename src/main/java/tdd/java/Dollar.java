6ff53d360998b2c544fa05f3f676ec0d780dd11e
igor.ferro.parreira@gmail.com <igor.ferro.parreira@gmail.com>
2020-09-08 12:10:18 -0300

    Equality for All, Redux

--- src/main/java/tdd/java/Dollar.java
+++ src/main/java/tdd/java/Dollar.java
@@ -1,20 +1,12 @@
 package tdd.java;
 
-public class Dollar {
-
-    private int amount;
+public class Dollar extends Money {
 
     public Dollar(int amount){
         this.amount =amount;
     }
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
-    }
-
-    public boolean equals(Object object){
-        Dollar dollar =  (Dollar) object;
-        return amount == dollar.amount;
-
     }
 
 }
--- src/main/java/tdd/java/Franc.java
+++ src/main/java/tdd/java/Franc.java
@@ -1,8 +1,6 @@
 package tdd.java;
 
-public class Franc {
-
-    private int amount;
+public class Franc extends Money {
 
     public Franc(int amount){
         this.amount =amount;
@@ -11,9 +9,4 @@ public class Franc {
         return new Franc(amount * multiplier);
     }
 
-    public boolean equals(Object object){
-        Franc franc =  (Franc) object;
-        return amount == franc.amount;
-
-    }
 }
