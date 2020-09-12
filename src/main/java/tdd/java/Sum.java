54f9491601738f695a91210d2817852d6e8e657c
igor.ferro.parreira@gmail.com <igor.ferro.parreira@gmail.com>
2020-09-12 04:46:08 -0300

    Create Sum Class

---
+++ src/main/java/tdd/java/Sum.java
@@ -0,0 +1,15 @@
+package tdd.java;
+
+public class Sum implements Expression {
+
+    Money augmend;
+    Money addmend;
+
+    public Sum(Money augmend, Money addmend) {
+        this.augmend = augmend;
+        this.addmend = addmend;
+    }
+
+
+
+}
