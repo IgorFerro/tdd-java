fe3ac42da1098fac41971347c6afd419b02540fb
igor.ferro.parreira@gmail.com <igor.ferro.parreira@gmail.com>
2020-09-13 18:09:52 -0300

    Add Pair class and other methods

--- src/main/java/tdd/java/Money.java
+++ src/main/java/tdd/java/Money.java
@@ -27,9 +27,9 @@ public class Money implements Expression {
     }
 
     @Override
-    public Money reduce(String to){
-        int rate = (currency.equals("CHF") && to.equals("USD")) ? 2:1;
-        return new Money(amount/rate, to);
+    public Money reduce(Bank bank, String to){
+       // int rate = (currency.equals("CHF") && to.equals("USD")) ? 2:1;
+        return new Money(amount / bank.rate(this.currency ,to), to);
     }
 
     @Override
